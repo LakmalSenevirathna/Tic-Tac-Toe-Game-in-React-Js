427d04badc22c302e78c782b04b9aa948b0efae3
Lakmal Senevirathna <laka.usjp@gmail.com>
2024-06-26 09:42:31 +0300

    Add toggle function to insert circle and cross icon to the bord

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,6 +1,31 @@
 import './TicTacToe.css'
+import circle from '../Assets/circle.png'
+import cross from '../Assets/cross.png'
+import { useState } from 'react';
+
+let data = ["","","","","","","","",""];
 
 const TicTacToe = () => {
+
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState (false);
+
+  const toggle = (e, num) => {
+    if (lock) {
+      return 0;
+    }
+    if (count%2 === 0) {
+      e.target.innerHTML = `<img src=${cross} >`;
+      data[num]="X";
+      setCount(++count);
+    }
+    else{
+      e.target.innerHTML = `<img src=${circle} >`;
+      data[num]="O";
+      setCount(++count);
+    }
+  }
+
   return (
     <div className='container'>
       <div className="header">
@@ -8,19 +33,19 @@ const TicTacToe = () => {
       </div>
       <div className="game__container">
         <div className="row__1">
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
+          <div className="box" onClick={(e) => {toggle(e,0)}} ></div>
+          <div className="box" onClick={(e) => {toggle(e,1)}}></div>
+          <div className="box" onClick={(e) => {toggle(e,2)}}></div>
         </div>
         <div className="row__2">
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
+          <div className="box" onClick={(e) => {toggle(e,3)}}></div>
+          <div className="box" onClick={(e) => {toggle(e,4)}}></div>
+          <div className="box" onClick={(e) => {toggle(e,5)}}></div>
         </div>
         <div className="row__3">
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
+          <div className="box" onClick={(e) => {toggle(e,6)}}></div>
+          <div className="box" onClick={(e) => {toggle(e,7)}}></div>
+          <div className="box" onClick={(e) => {toggle(e,8)}}></div>
         </div>
       </div>
       <div className="reset__container">
